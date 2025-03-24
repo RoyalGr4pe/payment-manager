007b2511dcb6fbfe23b404ee4937591c50293cc2
RoyalGr4pe <njames.programming@gmail.com>
2025-03-24 14:14:00 +0000

    remove uvicorn

--- app.py
+++ app.py
@@ -292,5 +292,5 @@ async def subscription_update(request: Request):
     return jsonify({"message": "Subscription Updated"}), 200
 
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+#if __name__ == "__main__":
+    #uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
